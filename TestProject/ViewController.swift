e4d2839582884bc3b4ede6dad3bf42acf6fcfb53
tsiomenkovitaliy <74013786+tsiomenkovitaliy@users.noreply.github.com>
2023-02-20 18:04:20 +0200

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Vitalii Tsiomenko on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
